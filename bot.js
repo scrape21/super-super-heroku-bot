6f332e97c22e9521be89883ee0418af19baa4157
scrape21 <43051446+scrape21@users.noreply.github.com>
2018-09-10 03:54:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Music = require('discord.js-musicbot-addon');
 
+Music.start(client, {
+  youtubeKey: 'AIzaSyAvE9uj8jqFWuBTbCPn3kucOKp6_zord2k' ,
+  prefix: "!",
+  leaveAlt: ["levi", "unicorns"],
+  helpCmd: "help"
+});
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.username}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+client.on('ready', () => { client.user.setGame('Little Nightmares') })
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
